723a2bcbf5740afe2926e3371dd9e4becb90efaf
jjh1129jh <jjh1129jh@naver.com>
2026-04-10 04:24:48 +0900

    두번쨰

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
-
 export default defineConfig({
   base: './', // 로컬/하위경로 배포 대비
   plugins: [
@@ -14,7 +13,10 @@ export default defineConfig({
       },
 
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+        // 캐싱할 파일 패턴
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        // ✅ PWA 캐시 용량 제한을 15MB로 상향 (기본값 2MB)
+        maximumFileSizeToCacheInBytes: 15 * 1024 * 1024 
       },
 
       includeAssets: ['apple-touch-icon.png'],
@@ -22,7 +24,7 @@ export default defineConfig({
         name: 'Coffee Shop',
         short_name: 'MyShop',
         description: '포트폴리오 페이지입니다.',
-        display:'standalone',
+        display: 'standalone',
         theme_color: '#000000',
         icons: [
           {
